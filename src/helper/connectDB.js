b12fc7a3730724bb05966914475c0bcb9211cfff
Prabhat <anteshkumar114@gmail.com>
2024-06-10 01:14:38 +0530

    new change8

--- src/helper/connectDB.js
+++ src/helper/connectDB.js
@@ -1,15 +1,6 @@
 import mongoose from "mongoose";
 const url = process.env.DB_URL;
 export const connectDb = async () => {
-  const connectionState = mongoose.connection.readyState;
-  if (connectionState == 1) {
-    console.log("already connected");
-    return;
-  }
-  if (connectionState == 2) {
-    console.log("Connecting...");
-    return;
-  }
   try {
     await mongoose.connect(url, {
       dbName: "npps",
