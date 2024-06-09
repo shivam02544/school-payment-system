18225918d3ed19b5038fdffd29c0a4f7627c16d4
Prabhat <anteshkumar114@gmail.com>
2024-06-09 23:41:08 +0530

    new change2

--- src/app/api/schoolDetail/route.js
+++ src/app/api/schoolDetail/route.js
@@ -5,5 +5,15 @@ import { NextResponse } from "next/server";
 export const GET = async () => {
   await connectDb();
   const schoolDetail = await SchoolDetail.findOne();
-  return NextResponse.json({ month: schoolDetail.billGeneratedMonth });
+  const response = NextResponse.json({
+    month: schoolDetail.billGeneratedMonth,
+  });
+  response.headers.set(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, proxy-revalidate"
+  );
+  response.headers.set("Pragma", "no-cache");
+  response.headers.set("Expires", "0");
+  response.headers.set("Surrogate-Control", "no-store");
+  return response;
 };
